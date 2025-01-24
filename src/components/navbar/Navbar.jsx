0512bec92cec7e3baa88d0c0f45ef5c6601e3b00
luisgcode <luisgcode@gmail.com>
2025-01-24 17:05:51 -0500

    Starting navigation menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,46 +1,40 @@
 import React from "react";
 import { heroIllustration, quickBookerLogo } from "../media";
+import { FaChevronDown } from "react-icons/fa";
+import { FaArrowRightToBracket } from "react-icons/fa6";
+
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className=" ">
-      <div className="   ">
-        {/* Logo */}
-        <div className=" ml-20 ">
-          <img src={quickBookerLogo} alt="Company logo" className="max-w-60" />
+    <div>
+      <div className=" text-sm top-nav px-[50px] py-2 flex items-center justify-between  ">
+        <div>
+          <button className="flex items-center gap-2">
+            Sign In <FaArrowRightToBracket />
+          </button>
         </div>
-
-        {/* Links */}
-        <div className="  ">
-          <a href="#features" className="hover:underline">
-            Features
-          </a>
-          <a href="#pricing" className="hover:underline">
-            Pricing
-          </a>
-          <a href="#use-cases" className="hover:underline">
-            Use Cases
-          </a>
-          <a href="#developers" className="hover:underline">
-            Developers
-          </a>{" "}
-          {/* CTA Buttons */}
-          <div className="space-x-4">
-            <button className="bg-btnBgColor border border-white px-4 py-2 rounded  ">
-              Log In
-            </button>
-            <button className="bg-white px-4 py-2 rounded text-btnBgColor uppercase ">
-              Sign UP
-            </button>
-          </div>
-        </div>
-
-        {/* Mobile Menu Icon */}
-        <div className="md:hidden">
-          <button className=" ">&#9776;</button>
+        <div className=" flex gap-10">
+          <button className="flex items-center gap-2">
+            Help Center <FaChevronDown />
+          </button>
+          <button className="flex items-center gap-2 font-semibold">
+            ENG <FaChevronDown />
+          </button>
         </div>
       </div>
-    </nav>
+      <nav>
+        <div className="p-sma_pad navigation flex items-center justify-between md:p-mid_pad">
+          <div className="logo">
+            <img
+              className="max-w-40"
+              src={quickBookerLogo}
+              alt="Company's logo"
+            />
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
