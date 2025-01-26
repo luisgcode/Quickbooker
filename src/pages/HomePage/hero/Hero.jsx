9391949147103b343d0d57dbcfb76d989cff03da
luisgcode <luisgcode@gmail.com>
2025-01-26 00:35:03 -0500

    Hero section

--- src/pages/HomePage/hero/Hero.jsx
+++ src/pages/HomePage/hero/Hero.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <header className=" bg-blue  py-20">
-      <div className="max-w-7xl mx-auto text-center">
-        <h1 className="text-4xl font-bold">
-          Scalable, Powerful, Online Booking API, Built for Modern Enterprises
-        </h1>
-        <p className="mt-4">
-          OnSched is the top booking framework used by Marketplaces,
-          Enterprises, and Digital Publishers.
-        </p>
-        <div className="mt-6">
-          <button className="bg-green-500 text-white px-6 py-2 rounded hover:bg-green-600">
-            Chat with Sales Team
-          </button>
+    <header className=" ">
+      <div className="  mx-auto text-center">
+        <div>
+          <h1 className=" ">
+            Scalable, Powerful, Online Booking API, Built for Modern Enterprises
+          </h1>
+          <p className=" ">
+            OnSched is the top booking framework used by Marketplaces,
+            Enterprises, and Digital Publishers.
+          </p>
+        </div>
+        <div>
+          <img src="" alt="" />
         </div>
       </div>
     </header>
