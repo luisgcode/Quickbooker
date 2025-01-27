d50d494004aaeb2013546475b9d703778b4748ea
luisgcode <luisgcode@gmail.com>
2025-01-26 23:57:31 -0500

    Fixing video elemtn

--- src/pages/HomePage/hero/Hero.jsx
+++ src/pages/HomePage/hero/Hero.jsx
@@ -8,20 +8,17 @@ const Hero = () => {
     if (videoRef.current) {
       videoRef.current.muted = true;
       videoRef.current.play().catch((error) => {
-        console.error(
-          "El navegador bloqueó la reproducción automática:",
-          error
-        );
+        console.error("error", error);
       });
     }
   }, []);
 
   return (
     <header>
-      <div className="flex gap-10 flex-col p-sma_pad md:py-[10px] md:px-[50px]    ">
-        <div className=" flex flex-col gap-6">
-          <h1 className=" ">Effortless Event Management for Businesses.</h1>
-          <p className=" ">
+      <div className="flex gap-10 flex-col p-sma_pad md:py-[10px] md:px-[50px]">
+        <div className="flex flex-col gap-6">
+          <h1>Effortless Event Management for Businesses.</h1>
+          <p>
             Streamline your bookings and grow your business with our powerful,
             easy-to-use platform designed for you.
           </p>
@@ -32,8 +29,11 @@ const Hero = () => {
             Get Started
           </button>
         </div>
-        <div className="   w-full">
-          <video ref={videoRef} loop muted src={promotionVideo}></video>
+        <div className="w-full">
+          <video ref={videoRef} loop muted>
+            <source src={promotionVideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
         </div>
       </div>
     </header>
