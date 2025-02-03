15fdb0ed13a59bde7f6ba504ad4003b5ba9310b2
Luis David Guaiquirian <72318958+luisgcode@users.noreply.github.com>
2025-02-03 14:22:37 -0500

    Creating login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import Homepage from "./pages/HomePage/Homepage";
+import LoginUserPage from "./pages/LoginUserPage/LoginUserPage";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<LoginUserPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/LoginUserPage/LoginUserPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginUserPage = () => {
+  return <div>this is login page</div>;
+};
+
+export default LoginUserPage;
