45b05399c3930dfcb8c558860752d316895a35f8
Luis David Guaiquirian <72318958+luisgcode@users.noreply.github.com>
2025-02-11 16:11:22 -0500

    working on the browser component

--- src/components/media.jsx
+++ src/components/media.jsx
@@ -1,18 +1,8 @@
 // QuickBooker logo
 import quickBookerLogo from "../assets/images/logo.svg";
 
-// Restaurant Covers
-import coverOne from "../assets/images/covers/rest-1.jpg";
-import coverTwo from "../assets/images/covers/rest-2.jpg";
-import coverThree from "../assets/images/covers/rest-3.jpg";
-import coverFour from "../assets/images/covers/rest-4.jpg";
-import coverFive from "../assets/images/covers/rest-5.jpg";
-import coverSix from "../assets/images/covers/rest-6.jpg";
-import coverSeven from "../assets/images/covers/rest-7.jpg";
-import coverEight from "../assets/images/covers/rest-8.jpg";
-import coverNine from "../assets/images/covers/rest-9.jpg";
-import coverTen from "../assets/images/covers/rest-10.jpg";
-import coverEleven from "../assets/images/covers/rest-11.jpg";
+// Venues types
+import VenueTypeMusic from "../assets/images/VenuesType/event-music.jpg";
 
 // Illustrations
 import heroIllustration from "../assets/images/illustration.svg";
@@ -20,4 +10,4 @@ import heroIllustration from "../assets/images/illustration.svg";
 // video
 import promotionVideo from "../assets/videos/promotion.mp4";
 
-export { quickBookerLogo, coverOne, heroIllustration, promotionVideo };
+export { quickBookerLogo, VenueTypeMusic, heroIllustration, promotionVideo };
