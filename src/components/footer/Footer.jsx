641ffe15ca41c9bd4b2da18ad686825f649d9b51
Luis David Guaiquirian <72318958+luisgcode@users.noreply.github.com>
2025-02-20 23:44:09 -0500

    Effect to footer items

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,44 +12,44 @@ const Footer = () => {
     <footer className="bg-footerColor p-sma_pad md:p-20  text-white  dark:text-black dark:bg-white dark:border-t-2">
       <nav className="flex flex-col  md:flex-row w-full gap-10 justify-between">
         {/* Support */}
-        <div className=" ">
-          <p className="font-semibold mb-6 flex gap-2">
-            <BiSupport />
-            Support
-          </p>
+          <div className=" ">
+            <p className="font-semibold mb-6 flex gap-2">
+              <BiSupport />
+              Support
+            </p>
 
-          <ul className="text-sm flex flex-col gap-2 ">
-            <li>
-              <a href="/faq">FAQ</a>
-            </li>
-            <li>
-              <a href="/contact">Contact Us</a>
-            </li>
-            <li>
-              <a href="/terms"> Conditions</a>
-            </li>
-            <li>
-              <a href="/privacy">Privacy Policy</a>
-            </li>
-          </ul>
-        </div>
-        {/* Discover */}
+            <ul className="text-sm flex flex-col gap-2 ">
+              <li className="hover:scale-110 transition-transform">
+                <a className="" href="/faq">FAQ</a>
+              </li>
+              <li className="hover:scale-110 transition-transform">
+                <a href="/contact">Contact Us</a>
+              </li>
+              <li className="hover:scale-110 transition-transform">
+                <a href="/terms"> Conditions</a>
+              </li>
+              <li className="hover:scale-110 transition-transform">
+                <a href="/privacy">Privacy Policy</a>
+              </li>
+            </ul>
+          </div>
+          {/* Discover */}
         <div className=" ">
           <p className="font-semibold mb-6 flex gap-2">
             <RiCompassDiscoverLine />
             Discover
           </p>
           <ul className="text-sm flex flex-col gap-2">
-            <li>
+          <li className="hover:scale-110 transition-transform">
               <a href="/spaces">Find a Space</a>
             </li>
-            <li>
+            <li className="hover:scale-110 transition-transform">
               <a href="/how-it-works">How It Works</a>
             </li>
-            <li>
+            <li className="hover:scale-110 transition-transform">
               <a href="/pricing">Pricing</a>
             </li>
-            <li>
+            <li className="hover:scale-110 transition-transform">
               <a href="/reviews">Customer Reviews</a>
             </li>
           </ul>
@@ -61,16 +61,16 @@ const Footer = () => {
             List your space
           </p>
           <ul className="text-sm flex flex-col gap-2">
-            <li>
+          <li className="hover:scale-110 transition-transform">
               <a href="/host">Become a Host</a>
             </li>
-            <li>
+            <li className="hover:scale-110 transition-transform">
               <a href="/host-guide">Hosting Guide</a>
             </li>
-            <li>
+            <li className="hover:scale-110 transition-transform">
               <a href="/safety">Safety & Security</a>
             </li>
-            <li>
+            <li className="hover:scale-110 transition-transform">
               <a href="/support">Host Support</a>
             </li>
           </ul>
@@ -82,16 +82,16 @@ const Footer = () => {
             Find us
           </p>
           <ul className="text-sm flex flex-col gap-2">
-            <li>
+          <li className="hover:scale-110 transition-transform">
               <a href="/host">Instagram</a>
             </li>
-            <li>
+            <li className="hover:scale-110 transition-transform">
               <a href="/host-guide">Linkedin</a>
             </li>
-            <li>
+            <li className="hover:scale-110 transition-transform">
               <a href="/safety">Youtube</a>
             </li>
-            <li>
+            <li className="hover:scale-110 transition-transform">
               <a href="/support">Facebook</a>
             </li>
           </ul>
@@ -103,19 +103,19 @@ const Footer = () => {
             Accesibility
           </p>
           <ul className="text-sm flex flex-col gap-2">
-            <li>
+          <li className="hover:scale-110 transition-transform">
               <a href="/host">Languages</a>
             </li>
-            <li>
+            <li className="hover:scale-110 transition-transform">
               <a href="/host-guide">Contrast</a>
             </li>
-            <li>
+            <li className="hover:scale-110 transition-transform">
               <a href="/safety">Text Size</a>
             </li>
-            <li>
+            <li className="hover:scale-110 transition-transform">
               <a href="/support">Screen Reader</a>
-            </li>{" "}
-            <li>
+            </li> 
+            <li className="hover:scale-110 transition-transform">
               <a href="/support">Guides</a>
             </li>
           </ul>
