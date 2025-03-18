b689183546758848a32f8f34d8d29fcca19b5a86
Luis David Guaiquirian <72318958+luisgcode@users.noreply.github.com>
2025-03-18 12:11:56 -0400

    feat(ui): working on Browse by venue component

--- src/components/media.jsx
+++ src/components/media.jsx
@@ -2,6 +2,12 @@
 import quickBookerLogo from "../assets/images/logo.svg";
 
 // Venues types
+import eventBall from "../assets/images/venues/event-ball.jpg"
+import eventBar from "../assets/images/venues/event-bar.jpg"
+import eventConference from "../assets/images/venues/event-conference.jpg"
+import eventWedding from "../assets/images/venues/event-wedding.jpg"
+import eventMusic from "../assets/images/venues/event-music.jpg"
+import eventwharehouse from "../assets/images/venues/event-wharehouse.jpg"
 
 // Illustrations
 import heroIllustration from "../assets/images/illustration.svg";
@@ -9,4 +15,4 @@ import heroIllustration from "../assets/images/illustration.svg";
 // video
 import promotionVideo from "../assets/videos/promotion.mp4";
 
-export { quickBookerLogo, heroIllustration, promotionVideo };
+export { quickBookerLogo, heroIllustration, promotionVideo, eventBall, eventBar, eventConference, eventWedding,eventwharehouse, eventMusic};
--- src/pages/HomePage/BrowseVenueType/BrowseVenueType.jsx
+++ src/pages/HomePage/BrowseVenueType/BrowseVenueType.jsx
@@ -1,10 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
+import { eventBall, eventMusic, eventConference, eventwharehouse, eventBar, eventWedding } from "../../../components/media";
+
+const venues = [
+  { name: "Montreal", properties: 1118, image: eventBall },
+  { name: "Quebec City", properties: 403, image: eventMusic },
+  { name: "Niagara Falls", properties: 253, image: eventConference },
+  { name: "Toronto", properties: 2027, image: eventWedding },
+  { name: "Vancouver", properties: 627, image: eventBar },
+  { name: "Calgary", properties: 482, image: eventwharehouse },
+];
 
 const BrowseVenueType = () => {
   return (
-    <div>
-      <div className="p-sma_pad md:p-mid_pad">
-        <h2>Browse by Venue Type goes here</h2>
+    <div className="p-sma_pad md:p-mid_pad">
+      <h2 className="text-2xl font-semibold mb-4">Browse by Venue Type</h2>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-6">
+        {venues.map((city, index) => (
+          <div key={index} className="text-center">
+            <img
+               src={city.image}
+              alt={city.name}
+              className="w-full h-40 object-cover rounded-lg shadow-md"
+            />
+            <h3 className="text-lg font-bold mt-2">{city.name}</h3>
+            <p className="text-gray-600">{city.properties} properties</p>
+          </div>
+        ))}
       </div>
     </div>
   );
