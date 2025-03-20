6870b355b5405a5da9e600bdce67b503960209e1
Luis David Guaiquirian <72318958+luisgcode@users.noreply.github.com>
2025-03-20 17:22:58 -0400

    updating

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/", // Asegúrate de que coincide con el nombre del repo en GitHub
 });
