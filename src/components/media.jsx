5c82e915ab3fcf0c2d65436b26ad5d9d1faf21ce
Luis David Guaiquirian <72318958+luisgcode@users.noreply.github.com>
2025-03-20 18:32:52 -0400

    fixing

--- src/components/media.jsx
+++ src/components/media.jsx
@@ -2,24 +2,24 @@
 import quickBookerLogo from "../assets/images/logo.svg";
 
 // Venues types
-import eventBall from "../assets/images/venues/event-ball.jpg"
-import eventBar from "../assets/images/venues/event-bar.jpg"
-import eventConference from "../assets/images/venues/event-conference.jpg"
-import eventWedding from "../assets/images/venues/event-wedding.jpg"
-import eventMusic from "../assets/images/venues/event-music.jpg"
-import eventGardens from "../assets/images/venues/event-garden.jpg"
+import eventBall from "../assets/images/venues/event-ball.jpg";
+import eventBar from "../assets/images/venues/event-bar.jpg";
+import eventConference from "../assets/images/venues/event-conference.jpg";
+import eventWedding from "../assets/images/venues/event-wedding.jpg";
+import eventMusic from "../assets/images/venues/event-music.jpg";
+import eventGardens from "../assets/images/venues/event-garden.jpg";
 
 // populars
-import bellamere from "../assets/images/venues/bellamere.jpg"
-import behance from "../assets/images/venues/behance.jpg"
-import cambridge from "../assets/images/venues/cambridge.jpg"
-import soundtrap from "../assets/images/venues/soundtrap.jpg"
+import bellamere from "../assets/images/venues/bellamere.jpg";
+import behance from "../assets/images/venues/Behance.jpg";
+import cambridge from "../assets/images/venues/cambridge.jpg";
+import soundtrap from "../assets/images/venues/soundtrap.jpg";
 
 // Public events
-import fifa from "../assets/images/venues/fifa.jpg"
-import clean from "../assets/images/venues/clean.png"
-import cricket from "../assets/images/venues/cricket.webp"
-import kpop from "../assets/images/venues/watching.webp"
+import fifa from "../assets/images/venues/fifa.jpg";
+import clean from "../assets/images/venues/clean.png";
+import cricket from "../assets/images/venues/cricket.webp";
+import kpop from "../assets/images/venues/watching.webp";
 
 // Illustrations
 import heroIllustration from "../assets/images/illustration.svg";
@@ -27,4 +27,22 @@ import heroIllustration from "../assets/images/illustration.svg";
 // video
 import promotionVideo from "../assets/videos/promotion.mp4";
 
-export {kpop, fifa, clean,cricket, bellamere, behance, cambridge, soundtrap, quickBookerLogo, heroIllustration, promotionVideo, eventBall, eventBar, eventConference, eventWedding,eventGardens, eventMusic};
+export {
+  kpop,
+  fifa,
+  clean,
+  cricket,
+  bellamere,
+  behance,
+  cambridge,
+  soundtrap,
+  quickBookerLogo,
+  heroIllustration,
+  promotionVideo,
+  eventBall,
+  eventBar,
+  eventConference,
+  eventWedding,
+  eventGardens,
+  eventMusic,
+};
